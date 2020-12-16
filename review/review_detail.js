3b97829afc09b33607999475b84a0d6ee0a7b26c
truuuely <kmyc7030@gmail.com>
2020-12-17 08:05:01 +0900

    no message

---
+++ review/review_detail.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.bxslider').bxSlider({
+        mode: 'horizontal', // 슬라이드 모드 ('fade', 'horizontal', 'vertical' 이 있음)
+        pager: true, // 페이지 표시 보여짐
+    });
+});
